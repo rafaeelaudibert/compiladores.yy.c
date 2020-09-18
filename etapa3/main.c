bca48b21cda0fd522549576446b3dddbb14695f2
RafaAudibert <rafaeelaudibert@gmail.com>
2020-09-17 23:45:23 -0300

    refactor(etapa3): Add decompiler on main.c

--- etapa3/main.c
+++ etapa3/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #include "lex.yy.h"
+#include "decompiler.h"
 
 // Fornecido pelo lexer em lex.yy.c
 extern char *yytext;
@@ -10,25 +11,35 @@ extern int yylex();
 
 // Fornecido pelo parser y.tab.c
 extern int yyparse();
+extern AST *ASTroot;
 
 void main(const int argc, const char **argv)
 {
+    FILE *outfile;
     initMe();
 
-    if (argc < 2)
+    if (argc < 3)
     {
-        fprintf(stderr, "Call: %s input\n", argv[0]);
+        fprintf(stderr, "Call: %s input output\n", argv[0]);
         exit(1);
     }
 
     yyin = fopen(argv[1], "r");
     if (yyin == NULL)
     {
-        fprintf(stderr, "Cannot open  file %s", argv[1]);
+        fprintf(stderr, "Cannot open input file %s", argv[1]);
+        exit(2);
+    }
+
+    outfile = fopen(argv[2], "w");
+    if (yyin == NULL)
+    {
+        fprintf(stderr, "Cannot open output file %s", argv[2]);
         exit(2);
     }
 
     yyparse();
+    decompile(ASTroot, outfile);
     fprintf(stdout, "Succesful Compilation!\n");
 
     exit(0);
