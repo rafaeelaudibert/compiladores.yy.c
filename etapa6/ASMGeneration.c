1ff7d6d3f1dfa32b8468d057978da8f9586abb33
RafaAudibert <rafaeelaudibert@gmail.com>
2020-11-17 20:58:34 -0300

    fix(etapa6): Fix usage of boolean literals in several places

--- etapa6/ASMGeneration.c
+++ etapa6/ASMGeneration.c
@@ -2,6 +2,11 @@
 #include "hash.h"
 #include "lex.yy.h"
 
+#define IS_ABSOLUTE(op) op->type != TK_IDENTIFIER && op->type != TYPE_TMP
+#define MOVL_ABSOLUTE_OR_VARIABLE_TO_EAX(op) IS_ABSOLUTE(op) ? "\tmovl\t$%s, %%eax\n" : "\tmovl\t_%s, %%eax\n"
+#define MOVL_ABSOLUTE_OR_VARIABLE_TO_EBX(op) IS_ABSOLUTE(op) ? "\tmovl\t$%s, %%edx\n" : "\tmovl\t_%s, %%ebx\n"
+#define PARSE_BOOLEAN_TO_STRING(op) op->type == LIT_TRUE ? "1" : (op->type == LIT_FALSE ? "0" : op->text)
+
 void generate_ASM(TAC *tac)
 {
     FILE *fout = fopen("out.s", "w");
@@ -39,49 +44,30 @@ void generate_ASM(TAC *tac)
         case TAC_COPY:
             fprintf(fout, "\t# TAC_COPY\n");
 
-            // Verify if we are copying from a variable or from an absolute value
-            fprintf(
-                fout,
-                tac->op1->type != TK_IDENTIFIER && tac->op1->type != TYPE_TMP
-                    ? "\tmovl\t$%s, %%eax\n"
-                      "\tmovl\t%%eax, _%s\n\n"
-                    : "\tmovl\t_%s, %%eax\n"
-                      "\tmovl\t%%eax, _%s\n\n",
-                tac->op1->text,
-                tac->res->text);
+            fprintf(fout, MOVL_ABSOLUTE_OR_VARIABLE_TO_EAX(tac->op1), tac->op1->text);
+            fprintf(fout, "\tmovl\t%%eax, _%s", tac->res->text);
 
             break;
         case TAC_COPY_IDX:
             fprintf(fout, "\t# TAC_COPY_IDX\n");
-            fprintf(fout,
-                    tac->op2->type != TK_IDENTIFIER && tac->op2->type != TYPE_TMP
-                        ? "\tmovl\t$%s, %%eax\n"
-                        : "\tmovl\t_%s, %%eax\n",
-                    tac->op2->text);
 
-            // Verify if we are copying from a variable or from an absolute value
-            fprintf(
-                fout,
-                tac->op1->type != TK_IDENTIFIER && tac->op1->type != TYPE_TMP
-                    ? "\tmovl\t$%s, %%edx\n"
-                      "\tmovl\t%%edx, _%s(,%%eax,4)\n\n"
-                    : "\tmovl\t_%s, %%edx\n"
-                      "\tmovl\t%%edx, _%s(,%%eax,4)\n\n",
-                tac->op1->text,
-                tac->res->text);
+            fprintf(fout, MOVL_ABSOLUTE_OR_VARIABLE_TO_EAX(tac->op2), tac->op2->text);
+            fprintf(fout, MOVL_ABSOLUTE_OR_VARIABLE_TO_EBX(tac->op1), tac->op1->text);
+            fprintf(fout, "\tmovl\t%%ebx, _%s(,%%eax,4)\n\n", tac->res->text);
 
             break;
         case TAC_BEGINFUN:
-            fprintf(fout,
-                    "\n\n\t.globl\t_%s\n"
-                    "_%s:\n"
-                    "\t# TAC_BEGINFUN\n"
-                    "\tpushl\t%%ebp\n"
-                    "\tmovl\t%%esp, %%ebp\n"
-                    "\tandl\t$-16, %%esp\n"
-                    "\tsubl\t$16, %%esp\n\n",
-                    tac->res->text,
-                    tac->res->text);
+            fprintf(
+                fout,
+                "\n\n\t.globl\t_%s\n"
+                "_%s:\n"
+                "\t# TAC_BEGINFUN\n"
+                "\tpushl\t%%ebp\n"
+                "\tmovl\t%%esp, %%ebp\n"
+                "\tandl\t$-16, %%esp\n"
+                "\tsubl\t$16, %%esp\n\n",
+                tac->res->text,
+                tac->res->text);
             break;
         case TAC_ENDFUN:
             fprintf(fout,
@@ -92,103 +78,51 @@ void generate_ASM(TAC *tac)
         case TAC_ADD:
             fprintf(fout, "\t# TAC_ADD\n");
 
-            fprintf(
-                fout,
-                tac->op1->type != TK_IDENTIFIER && tac->op1->type != TYPE_TMP
-                    ? "\tmovl\t$%s, %%edx\n"
-                    : "\tmovl\t_%s, %%edx\n",
-                tac->op1->text);
-            fprintf(
-                fout,
-                tac->op2->type != TK_IDENTIFIER && tac->op2->type != TYPE_TMP
-                    ? "\tmovl\t$%s, %%eax\n"
-                    : "\tmovl\t_%s, %%eax\n",
-                tac->op2->text);
-
+            fprintf(fout, MOVL_ABSOLUTE_OR_VARIABLE_TO_EBX(tac->op1), tac->op1->text);
+            fprintf(fout, MOVL_ABSOLUTE_OR_VARIABLE_TO_EAX(tac->op2), tac->op2->text);
             fprintf(fout,
-                    "\taddl\t%%edx, %%eax\n"
+                    "\taddl\t%%ebx, %%eax\n"
                     "\tmovl\t%%eax, _%s\n\n",
                     tac->res->text);
             break;
         case TAC_SUB:
             fprintf(fout, "\t# TAC_SUB\n");
 
-            fprintf(
-                fout,
-                tac->op1->type != TK_IDENTIFIER && tac->op1->type != TYPE_TMP
-                    ? "\tmovl\t$%s, %%edx\n"
-                    : "\tmovl\t_%s, %%edx\n",
-                tac->op1->text);
-            fprintf(
-                fout,
-                tac->op2->type != TK_IDENTIFIER && tac->op2->type != TYPE_TMP
-                    ? "\tmovl\t$%s, %%eax\n"
-                    : "\tmovl\t_%s, %%eax\n",
-                tac->op2->text);
-
+            fprintf(fout, MOVL_ABSOLUTE_OR_VARIABLE_TO_EBX(tac->op1), tac->op1->text);
+            fprintf(fout, MOVL_ABSOLUTE_OR_VARIABLE_TO_EAX(tac->op2), tac->op2->text);
             fprintf(fout,
-                    "\tsubl\t%%edx, %%eax\n"
+                    "\tsubl\t%%ebx, %%eax\n"
                     "\tmovl\t%%eax, _%s\n\n",
                     tac->res->text);
             break;
         case TAC_MULTIPLY:
             fprintf(fout, "\t# TAC_MULTIPLY\n");
 
-            fprintf(
-                fout,
-                tac->op1->type != TK_IDENTIFIER && tac->op1->type != TYPE_TMP
-                    ? "\tmovl\t$%s, %%edx\n"
-                    : "\tmovl\t_%s, %%edx\n",
-                tac->op1->text);
-            fprintf(
-                fout,
-                tac->op2->type != TK_IDENTIFIER && tac->op2->type != TYPE_TMP
-                    ? "\tmovl\t$%s, %%eax\n"
-                    : "\tmovl\t_%s, %%eax\n",
-                tac->op2->text);
-
+            fprintf(fout, MOVL_ABSOLUTE_OR_VARIABLE_TO_EBX(tac->op1), tac->op1->text);
+            fprintf(fout, MOVL_ABSOLUTE_OR_VARIABLE_TO_EAX(tac->op2), tac->op2->text);
             fprintf(fout,
-                    "\timull\t%%edx, %%eax\n"
+                    "\timull\t%%ebx, %%eax\n"
                     "\tmovl\t%%eax, _%s\n\n",
                     tac->res->text);
             break;
         case TAC_DIVIDE:
             fprintf(fout, "\t# TAC_DIVIDE\n");
 
-            fprintf(
-                fout,
-                tac->op1->type != TK_IDENTIFIER && tac->op1->type != TYPE_TMP
-                    ? "\tmovl\t$%s, %%eax\n"
-                    : "\tmovl\t_%s, %%eax\n",
-                tac->op1->text);
-            fprintf(
-                fout,
-                tac->op2->type != TK_IDENTIFIER && tac->op2->type != TYPE_TMP
-                    ? "\tmovl\t$%s, %%ecx\n"
-                    : "\tmovl\t_%s, %%ecx\n",
-                tac->op2->text);
-
+            fprintf(fout, MOVL_ABSOLUTE_OR_VARIABLE_TO_EAX(tac->op1), tac->op1->text);
+            fprintf(fout, MOVL_ABSOLUTE_OR_VARIABLE_TO_EBX(tac->op2), tac->op2->text);
             fprintf(fout,
                     "\tcltd\n"
-                    "\tidivl\t%%ecx\n"
+                    "\tidivl\t%%ebx\n"
                     "\tmovl\t%%eax, _%s\n\n",
                     tac->res->text);
             break;
         case TAC_LT:
             fprintf(fout, "\t# TAC_LT\n");
 
+            fprintf(fout, MOVL_ABSOLUTE_OR_VARIABLE_TO_EAX(tac->op2), tac->op2->text);
             fprintf(fout,
-                    tac->op2->type != TK_IDENTIFIER && tac->op2->type != TYPE_TMP
-                        ? "\tmovl\t$%s, %%eax\n"
-                        : "\tmovl\t_%s, %%eax\n",
-                    tac->op2->text);
-
-            fprintf(fout,
-                    tac->op1->type != TK_IDENTIFIER && tac->op1->type != TYPE_TMP
-                        ? "\tcmpl\t$%s, %%eax\n"
-                        : "\tcmpl\t_%s, %%eax\n",
+                    IS_ABSOLUTE(tac->op1) ? "\tcmpl\t$%s, %%eax\n" : "\tcmpl\t_%s, %%eax\n",
                     tac->op1->text);
-
             fprintf(fout,
                     "\tsetg\t%%al\n"
                     "\tmovzbl\t%%al, %%eax\n"
@@ -198,18 +132,10 @@ void generate_ASM(TAC *tac)
         case TAC_LE:
             fprintf(fout, "\t# TAC_LE\n");
 
+            fprintf(fout, MOVL_ABSOLUTE_OR_VARIABLE_TO_EAX(tac->op1), tac->op1->text);
             fprintf(fout,
-                    tac->op1->type != TK_IDENTIFIER && tac->op1->type != TYPE_TMP
-                        ? "\tmovl\t$%s, %%eax\n"
-                        : "\tmovl\t_%s, %%eax\n",
-                    tac->op1->text);
-
-            fprintf(fout,
-                    tac->op2->type != TK_IDENTIFIER && tac->op2->type != TYPE_TMP
-                        ? "\tcmpl\t$%s, %%eax\n"
-                        : "\tcmpl\t_%s, %%eax\n",
+                    IS_ABSOLUTE(tac->op2) ? "\tcmpl\t$%s, %%eax\n" : "\tcmpl\t_%s, %%eax\n",
                     tac->op2->text);
-
             fprintf(fout,
                     "\tsetle\t%%al\n"
                     "\tmovzbl\t%%al, %%eax\n"
@@ -219,18 +145,10 @@ void generate_ASM(TAC *tac)
         case TAC_GT:
             fprintf(fout, "\t# TAC_GT\n");
 
+            fprintf(fout, MOVL_ABSOLUTE_OR_VARIABLE_TO_EAX(tac->op1), tac->op1->text);
             fprintf(fout,
-                    tac->op1->type != TK_IDENTIFIER && tac->op1->type != TYPE_TMP
-                        ? "\tmovl\t$%s, %%eax\n"
-                        : "\tmovl\t_%s, %%eax\n",
-                    tac->op1->text);
-
-            fprintf(fout,
-                    tac->op2->type != TK_IDENTIFIER && tac->op2->type != TYPE_TMP
-                        ? "\tcmpl\t$%s, %%eax\n"
-                        : "\tcmpl\t_%s, %%eax\n",
+                    IS_ABSOLUTE(tac->op2) ? "\tcmpl\t$%s, %%eax\n" : "\tcmpl\t_%s, %%eax\n",
                     tac->op2->text);
-
             fprintf(fout,
                     "\tsetg\t%%al\n"
                     "\tmovzbl\t%%al, %%eax\n"
@@ -240,18 +158,10 @@ void generate_ASM(TAC *tac)
         case TAC_GE:
             fprintf(fout, "\t# TAC_GE\n");
 
+            fprintf(fout, MOVL_ABSOLUTE_OR_VARIABLE_TO_EAX(tac->op2), tac->op2->text);
             fprintf(fout,
-                    tac->op2->type != TK_IDENTIFIER && tac->op2->type != TYPE_TMP
-                        ? "\tmovl\t$%s, %%eax\n"
-                        : "\tmovl\t_%s, %%eax\n",
-                    tac->op2->text);
-
-            fprintf(fout,
-                    tac->op1->type != TK_IDENTIFIER && tac->op1->type != TYPE_TMP
-                        ? "\tcmpl\t$%s, %%eax\n"
-                        : "\tcmpl\t_%s, %%eax\n",
+                    IS_ABSOLUTE(tac->op1) ? "\tcmpl\t$%s, %%eax\n" : "\tcmpl\t_%s, %%eax\n",
                     tac->op1->text);
-
             fprintf(fout,
                     "\tsetle\t%%al\n"
                     "\tmovzbl\t%%al, %%eax\n"
@@ -263,63 +173,31 @@ void generate_ASM(TAC *tac)
             fprintf(fout, "\t# TAC_OR\n");
 
             fprintf(fout,
-                    tac->op1->type != TK_IDENTIFIER && tac->op1->type != TYPE_TMP
-                        ? "\tmov\t$%s, %%al\n"
-                        : "\tmov\t_%s, %%al\n",
-                    tac->op1->type == LIT_TRUE
-                        ? "1"
-                        : (tac->op1->type == LIT_FALSE ? "0" : tac->op1->text));
+                    IS_ABSOLUTE(tac->op1) ? "\tmov\t$%s, %%al\n" : "\tmov\t_%s, %%al\n",
+                    PARSE_BOOLEAN_TO_STRING(tac->op1));
             fprintf(fout,
-                    tac->op2->type != TK_IDENTIFIER && tac->op2->type != TYPE_TMP
-                        ? "\tmov\t$%s, %%bl\n"
-                        : "\tmov\t_%s, %%bl\n",
-                    tac->op2->type == LIT_TRUE
-                        ? "1"
-                        : (tac->op2->type == LIT_FALSE ? "0" : tac->op2->text));
-            fprintf(fout, "\tor\t%%bl, %%al\n"); // eax = eax || ebx
-
-            fprintf(fout,
-                    "\tmov\t%%al, _%s\n\n",
-                    tac->res->text);
+                    IS_ABSOLUTE(tac->op2) ? "\tmov\t$%s, %%bl\n" : "\tmov\t_%s, %%bl\n",
+                    PARSE_BOOLEAN_TO_STRING(tac->op2));
+            fprintf(fout, "\tor\t%%bl, %%al\n"); // al = al || bl
+            fprintf(fout, "\tmov\t%%al, _%s\n\n", tac->res->text);
             break;
         case TAC_AND:
             fprintf(fout, "\t# TAC_AND\n");
 
             fprintf(fout,
-                    tac->op1->type != TK_IDENTIFIER && tac->op1->type != TYPE_TMP
-                        ? "\tmov\t$%s, %%al\n"
-                        : "\tmov\t_%s, %%al\n",
-                    tac->op1->type == LIT_TRUE
-                        ? "1"
-                        : (tac->op1->type == LIT_FALSE ? "0" : tac->op1->text));
+                    IS_ABSOLUTE(tac->op1) ? "\tmov\t$%s, %%al\n" : "\tmov\t_%s, %%al\n",
+                    PARSE_BOOLEAN_TO_STRING(tac->op1));
             fprintf(fout,
-                    tac->op2->type != TK_IDENTIFIER && tac->op2->type != TYPE_TMP
-                        ? "\tmov\t$%s, %%bl\n"
-                        : "\tmov\t_%s, %%bl\n",
-                    tac->op2->type == LIT_TRUE
-                        ? "1"
-                        : (tac->op2->type == LIT_FALSE ? "0" : tac->op2->text));
-            fprintf(fout, "\tand\t%%bl, %%al\n"); // eax = eax && ebx
-
-            fprintf(fout,
-                    "\tmov\t%%al, _%s\n\n",
-                    tac->res->text);
+                    IS_ABSOLUTE(tac->op2) ? "\tmov\t$%s, %%bl\n" : "\tmov\t_%s, %%bl\n",
+                    PARSE_BOOLEAN_TO_STRING(tac->op2));
+            fprintf(fout, "\tand\t%%bl, %%al\n"); // al = al && bl
+            fprintf(fout, "\tmov\t%%al, _%s\n\n", tac->res->text);
             break;
         case TAC_EQ:
             fprintf(fout, "\t# TAC_EQ\n");
 
-            fprintf(fout,
-                    tac->op1->type != TK_IDENTIFIER && tac->op1->type != TYPE_TMP
-                        ? "\tmovl\t$%s, %%eax\n"
-                        : "\tmovl\t_%s, %%eax\n",
-                    tac->op1->text);
-
-            fprintf(fout,
-                    tac->op2->type != TK_IDENTIFIER && tac->op2->type != TYPE_TMP
-                        ? "\tmovl\t$%s, %%ebx\n"
-                        : "\tmovl\t_%s, %%ebx\n",
-                    tac->op2->text);
-
+            fprintf(fout, MOVL_ABSOLUTE_OR_VARIABLE_TO_EAX(tac->op1), tac->op1->text);
+            fprintf(fout, MOVL_ABSOLUTE_OR_VARIABLE_TO_EBX(tac->op2), tac->op2->text);
             fprintf(fout,
                     "\tcmpl\t%%eax, %%ebx\n"
                     "\tsete\t%%al\n"
@@ -330,18 +208,8 @@ void generate_ASM(TAC *tac)
         case TAC_DIF:
             fprintf(fout, "\t# TAC_DIF\n");
 
-            fprintf(fout,
-                    tac->op1->type != TK_IDENTIFIER && tac->op1->type != TYPE_TMP
-                        ? "\tmovl\t$%s, %%eax\n"
-                        : "\tmovl\t_%s, %%eax\n",
-                    tac->op1->text);
-
-            fprintf(fout,
-                    tac->op2->type != TK_IDENTIFIER && tac->op2->type != TYPE_TMP
-                        ? "\tmovl\t$%s, %%ebx\n"
-                        : "\tmovl\t_%s, %%ebx\n",
-                    tac->op2->text);
-
+            fprintf(fout, MOVL_ABSOLUTE_OR_VARIABLE_TO_EAX(tac->op1), tac->op1->text);
+            fprintf(fout, MOVL_ABSOLUTE_OR_VARIABLE_TO_EBX(tac->op2), tac->op2->text);
             fprintf(fout,
                     "\tcmpl\t%%eax, %%ebx\n"
                     "\tsetne\t%%al\n"
@@ -352,12 +220,7 @@ void generate_ASM(TAC *tac)
         case TAC_UNARY_MINUS:
             fprintf(fout, "\t# TAC_UNARY_MINUS\n");
 
-            fprintf(fout,
-                    tac->op1->type != TK_IDENTIFIER && tac->op1->type != TYPE_TMP
-                        ? "\tmovl\t$%s, %%eax\n"
-                        : "\tmovl\t_%s, %%eax\n",
-                    tac->op1->text);
-
+            fprintf(fout, MOVL_ABSOLUTE_OR_VARIABLE_TO_EAX(tac->op1), tac->op1->text);
             fprintf(fout,
                     "\tnegl\t%%eax\n"
                     "\tmovl\t%%eax, _%s\n\n",
@@ -366,13 +229,10 @@ void generate_ASM(TAC *tac)
         case TAC_UNARY_NEGATION:
             fprintf(fout, "\t# TAC_UNARY_NEGATION\n");
 
-            fprintf(fout,
-                    tac->op1->type != TK_IDENTIFIER && tac->op1->type != TYPE_TMP
-                        ? "\tmov\t$%s, %%al\n"
-                        : "\tmov\t_%s, %%al\n",
-                    tac->op1->text);
-
             // Negating a boolean, is the same as bool (xor) 1
+            fprintf(fout,
+                    IS_ABSOLUTE(tac->op1) ? "\tmov\t$%s, %%al\n" : "\tmov\t_%s, %%al\n",
+                    PARSE_BOOLEAN_TO_STRING(tac->op1));
             fprintf(fout,
                     "\tmov\t$1, %%bl\n"
                     "\txor\t%%bl, %%al\n"
@@ -386,26 +246,16 @@ void generate_ASM(TAC *tac)
         case TAC_JUMPFALSE:
             fprintf(fout, "\t# TAC_JUMPFALSE\n");
 
-            fprintf(fout,
-                    tac->op1->type != TK_IDENTIFIER && tac->op1->type != TYPE_TMP
-                        ? "\tmovl\t$%s, %%eax\n"
-                        : "\tmovl\t_%s, %%eax\n",
-                    tac->op1->text);
+            fprintf(fout, MOVL_ABSOLUTE_OR_VARIABLE_TO_EAX(tac->op1), PARSE_BOOLEAN_TO_STRING(tac->op1));
             fprintf(fout,
                     "\tmovl\t$1, %%ebx\n"
                     "\tcmpl\t%%eax, %%ebx\n");
-
             fprintf(fout, "\tjne\t_%s\n\n", tac->res->text);
             break;
         case TAC_RETURN:
             fprintf(fout, "\t# TAC_RETURN\n");
 
-            fprintf(fout,
-                    tac->op1->type != TK_IDENTIFIER && tac->op1->type != TYPE_TMP
-                        ? "\tmovl\t$%s, %%eax\n"
-                        : "\tmovl\t_%s, %%eax\n",
-                    tac->op1->text);
-
+            fprintf(fout, MOVL_ABSOLUTE_OR_VARIABLE_TO_EAX(tac->op1), tac->op1->text);
             fprintf(fout, "\tret\n\n");
             break;
         case TAC_PRINT:
@@ -419,6 +269,10 @@ void generate_ASM(TAC *tac)
                 char *escaped_str = escaped_string(tac->op1->text);
                 fprintf(fout, "\tmovl\t$__%s_string_r4nd0m, %%eax\n", escaped_str);
                 print_int = FALSE;
+            }
+            else if (tac->op1->type == LIT_FALSE || tac->op1->type == LIT_TRUE)
+            {
+                fprintf(fout, "\tmovl\t$%s, %%eax\n", PARSE_BOOLEAN_TO_STRING(tac->op1));
             }
             else if (tac->op1->type != TK_IDENTIFIER && tac->op1->type != TYPE_TMP)
             {
